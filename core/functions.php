e6cfd9ed0d86d5aea148bfa27f162ae5a120af23
Christian Arevalo <carevaloq87@hotmail.com>
2017-01-09 00:24:52 +0000

    Add Dandenong office

--- core/functions.php
+++ core/functions.php
@@ -35,7 +35,7 @@ function find_matter_type_in_body($body ,$event_info, $location, $date_time) {
             $output['event_type'] = "VLA Appointment Reminder";
             $output['event_template'] = vla_apoint_reminder_template($event_info, $location, $date_time);
             break;
-        case stristr($body,'(SAR)'):
+        case stristr($body,'(SPR)'):
             $output['event_type'] = "Specialist appointment reminder";
             $output['event_template'] = specialist_appointment_reminder_template($event_info, $location, $date_time);
             break;
@@ -61,6 +61,18 @@ function find_matter_type_in_body($body ,$event_info, $location, $date_time) {
             $output['phone']          = sanitize_phone($event_info[1]);
 	    $output['event_type']     = "Criminal Law Appointment Reminder";
             $output['event_template'] = criminal_law_appointment_reminder_template($event_info, $location, $date_time);
+            break;
+	case stristr($body,'(SAR)'):
+            $output['client_name']    = (sizeof($event_info) > 1 ? $event_info[0]: "-");
+            $output['phone']          = sanitize_phone($event_info[1]);
+	    $output['event_type']     = "Sunshine Appointment Reminder";
+            $output['event_template'] = sunshine_appointment_reminder_template($event_info, $location, $date_time);
+            break;
+	case stristr($body,'(DAR)'):
+            $output['client_name']    = (sizeof($event_info) > 1 ? $event_info[0]: "-");
+            $output['phone']          = sanitize_phone($event_info[1]);
+	    $output['event_type']     = "Dandenong Appointment Reminder";
+            $output['event_template'] = dandenong_appointment_reminder_template($event_info, $location, $date_time);
             break;
         default:
             # code...
@@ -199,6 +211,30 @@ function criminal_law_appointment_reminder_template($event_info, $location, $dat
     $date   = $date_time['date'];
     $time   = $date_time['time'];
     return "You have an appointment on " . $date . " at " . $time . " with Victoria Legal Aid. Location of appointment is at " . $location . ". To change call us on " . $phone . ".";  
+}
+
+/**
+ * Sunshine appointment reminders template
+ * @param  Array $event_info  
+ * @param  String $date_time  Date and time of the event
+ * @return String             Sunshine appointment reminder message template with all the information provided
+ */
+function sunshine_appointment_reminder_template($event_info, $location, $date_time) {
+    $date   = $date_time['date'];
+    $time   = $date_time['time'];
+    return "You have an appointment on " . $date . " at " . $time . " with Victoria Legal Aid. Location of appointment is at " . $location . ". To change call us on 9300 5333."; 
+}
+
+/**
+ * Dandenong appointment reminders template
+ * @param  Array $event_info  
+ * @param  String $date_time  Date and time of the event
+ * @return String             Dandenong appointment reminder message template with all the information provided
+ */
+function dandenong_appointment_reminder_template($event_info, $location, $date_time) {
+    $date   = $date_time['date'];
+    $time   = $date_time['time'];
+    return "You have an appointment on " . $date . " at " . $time . " with Victoria Legal Aid. Location of appointment is at " . $location . ". To change call us on 9767 7111."; 
 }
 
 /**
