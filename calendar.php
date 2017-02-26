7e6479f0d79717ecab8cac80471e605ae645f759
codeforvic <codeforvic@codeforvic.vla.vic.gov.au>
2017-02-27 10:16:18 +1100

    meta for IE x-ua-compatibility added

--- calendar.php
+++ calendar.php
@@ -12,6 +12,7 @@
   <head>
     <title>PHP Calendar API Tutorial</title>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <!-- <link href="css/font-awesome.min.css" rel="stylesheet"> -->
